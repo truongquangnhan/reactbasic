ddf34d96365cb2ff89c04f9b3d5f18c2b891b9e0
nhantq <truongquangnhan321@gmail.com>
2022-10-02 22:55:55 +0700

    Demo state

--- src/views/example/Example.js
+++ src/views/example/Example.js
@@ -6,11 +6,20 @@ class Example extends React.Component {
         longName: "Con mọe nó"
     }
 
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
     render() {
         return (
             <>
+                <p>INPUT NAME: </p>
+                <input value={this.state.name} type={"text"} onChange={this.handleOnChangeName}/>
+                <br/>
                 <h6>Hi {this.state.name}</h6>
-            <p className="mycss-class">Hello chú em chú muốn nói: {this.state.longName}</p>
+                <p className="mycss-class">Hello chú em chú muốn nói: {this.state.longName}</p>
             </>
         );
     }
